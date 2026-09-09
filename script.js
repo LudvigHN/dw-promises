02abd64fa90e2a764314f5b1b28181fd916084fb
LudvigHN <ludvighaukrognielsen@gmail.com>
2026-09-09 13:43:50 +0200

    step 4

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+const listDOM = document.querySelector(".people")
+
 fetch("./data/people.json")
     .then(response => response.json())
     .then(data =>{
-        console.log(data);
+        listDOM.innerHTML = data.people.map(person => `
+                    <li>
+                    ${person.name} ${person.age} år
+                    </li>
+                    `)
+                    .join("")
         
     })
