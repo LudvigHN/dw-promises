dea21372e1c14926a92172e6a92813c218f554dc
LudvigHN <ludvighaukrognielsen@gmail.com>
2026-09-09 13:46:13 +0200

    step 5

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const listDOM = document.querySelector(".people")
 
 fetch("./data/people.json")
-    .then(response => response.json())
-    .then(data =>{
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Kunne ikke hente data");
+        }
+        return response.json();
+    })
+    .then(data => {
         listDOM.innerHTML = data.people.map(person => `
                     <li>
                     ${person.name} ${person.age} år
                     </li>
                     `)
-                    .join("")
+            .join("")
+
+    })
+    .catch(error =>{
+        console.error("Fejl:",error);
         
     })
